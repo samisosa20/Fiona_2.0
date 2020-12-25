599c1af8cfcb6d5c973666a7a029baff217ecd5f
Deiwin Ignacio Monsalve Altamar <deiwin1987@gmail.com>
2020-12-25 15:03:28 -0500

    arreglando warnings

--- src/redux/actions/Login.js
+++ src/redux/actions/Login.js
@@ -2,7 +2,7 @@ export const ADD_USER = "ADD_USER";
 export const ADD_PSSD = "ADD_PSSD";
 
 export const return_user = (name, value) => {
-  if (name == "user") {
+  if (name === "user") {
     return {
       type: ADD_USER,
       user: value,
