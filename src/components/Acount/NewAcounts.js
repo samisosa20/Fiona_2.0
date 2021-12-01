094208517221f090027a7b67753e7267d293f484
Sammy Guttman <1449@holbertonschool.com>
2021-12-01 11:48:03 -0500

    create context menu in account page

--- src/components/Acount/NewAcounts.js
+++ src/components/Acount/NewAcounts.js
@@ -1,15 +1,18 @@
 import React from "react";
-
-import { Card, CardBody, CardHeader, Button, Row } from "reactstrap";
+import { ContextMenuTrigger } from "react-contextmenu";
 import { Link } from "react-router-dom";
 
-const AcountAdd = props => {
+import { Card, CardBody, CardHeader, Button, Row } from "reactstrap";
+
+import ContextMenuCustom from "../ContextMenu";
+
+const AcountAdd = (props) => {
   const {
     state,
     OpenModalNew,
     OpenModalEdit,
     OpenModalDelete,
-    OpenModalShare
+    OpenModalShare,
   } = props;
   const idc = localStorage.getItem("IdUser");
   return (
@@ -17,108 +20,70 @@ const AcountAdd = props => {
       <Row className="col-12 justify-content-between p-0 m-0">
         {state
           ? state.map((data, index) => (
-              <Card className="shadow col-md-6 mb-3" key={index}>
-                <CardHeader className="border-0">
-                  <Row>
-                    <div className="col p-0">
-                      <h3 className="mb-0">{data.nombre}</h3>
-                    </div>
-                    {data.cantidad_int < 0 ? (
-                      <div className="col justify-content-end text-danger">
-                        $ {data.cantidad}
-                      </div>
-                    ) : (
-                      <div className="col justify-content-end text-success">
-                        $ {data.cantidad}
-                      </div>
-                    )}
-                  </Row>
-                  <Row>
-                    <div className="col p-0">Badge: {data.divisa}</div>
-                    <div className="col p-0">
-                      {data.cuenta_ahorro === "1"
-                        ? "Saving Acount"
-                        : data.propietario &&
-                          data.propietario !== idc && (
-                            <div className="text-danger">Shared Account</div>
-                          )}
-                    </div>
-                  </Row>
-                </CardHeader>
-                <CardBody className="mt--4">
-                  <Row className="col m-0 justify-content-md-start justify-content-around p-0">
-                    <Link
-                      to={
-                        "/admin/move?acount=" + data.id + "&naco=" + data.nombre
-                      }
-                    >
-                      <Button
-                        className="shadow btn-circle mr-md-2"
-                        color="success"
-                        size="sm"
-                        title="come in"
-                      >
-                        <i className="fas fa-sign-in-alt"></i>
-                      </Button>
-                    </Link>
-                    {data.propietario === idc || data.propietario === null ? (
-                      <Button
-                        className="shadow btn-circle"
-                        color="info"
-                        size="sm"
-                        title="edit"
-                        onClick={e =>
-                          OpenModalEdit(
-                            e,
-                            data.id,
-                            data.nombre,
-                            data.descripcion,
-                            data.divisa,
-                            data.monto_inicial,
-                            data.cuenta_ahorro
-                          )
-                        }
-                      >
-                        <i className="fas fa-pencil-alt"></i>
-                      </Button>
-                    ) : (
-                      ""
-                    )}
-                    {data.propietario === idc || data.propietario === null ? (
-                      <Button
-                        className="shadow btn-circle"
-                        color="danger"
-                        size="sm"
-                        title="Delete"
-                        onClick={e => OpenModalDelete(e, data.id, data.nombre)}
-                      >
-                        <i className="far fa-trash-alt white"></i>
-                      </Button>
-                    ) : (
-                      ""
-                    )}
-                    {data.propietario === idc || data.propietario === null ? (
-                      <Button
-                        className="shadow btn-circle"
-                        color="warning"
-                        size="sm"
-                        title="Share"
-                        onClick={e => OpenModalShare(e, data.id, data.nombre)}
-                      >
-                        <i className="fas fa-share"></i>
-                      </Button>
-                    ) : (
-                      ""
-                    )}
-                  </Row>
-                </CardBody>
-              </Card>
+              <Link
+                to={"/admin/move?acount=" + data.id + "&naco=" + data.nombre}
+                className="col-md-6 mb-3 text-decoration-none"
+              >
+                <Card
+                  className="col-12 cursor-pointer hover:shadow"
+                  key={index}
+                >
+                  <ContextMenuTrigger id={`context-account-${index}`}>
+                    <CardHeader className="border-0">
+                      <Row>
+                        <div className="col p-0">
+                          <h3 className="mb-0">{data.nombre}</h3>
+                        </div>
+                        {data.cantidad_int < 0 ? (
+                          <div className="col justify-content-end text-danger">
+                            $ {data.cantidad}
+                          </div>
+                        ) : (
+                          <div className="col justify-content-end text-success">
+                            $ {data.cantidad}
+                          </div>
+                        )}
+                      </Row>
+                      <Row>
+                        <div className="col p-0 text-dark">Badge: {data.divisa}</div>
+                        <div className="col p-0 text-muted">
+                          {data.cuenta_ahorro === "1"
+                            ? "Saving Acount"
+                            : data.propietario &&
+                              data.propietario !== idc && (
+                                <div className="text-danger">
+                                  Shared Account
+                                </div>
+                              )}
+                        </div>
+                      </Row>
+                    </CardHeader>
+                  </ContextMenuTrigger>
+                  <ContextMenuCustom
+                    idContext={`context-account-${index}`}
+                    onClickShare={(e) =>
+                      OpenModalShare(e, data.id, data.nombre)
+                    }
+                    onClickEdit={(e) =>
+                      OpenModalEdit(
+                        e,
+                        data.id,
+                        data.nombre,
+                        data.descripcion,
+                        data.divisa,
+                        data.monto_inicial,
+                        data.cuenta_ahorro
+                      )
+                    }
+                    onClickDelete={(e) =>
+                      OpenModalDelete(e, data.id, data.nombre)
+                    }
+                  />
+                </Card>
+              </Link>
             ))
           : ""}
-        <Card
-          className="shadow col-md-6 mb-3"
-          onClick={e => OpenModalNew(e)}
-        >
+        <Card className="shadow col-md-6 mb-3" onClick={(e) => OpenModalNew(e)}>
           <CardBody style={{ paddingLeft: "10px", paddingRight: "10px" }}>
             <Row>
               <div className="col-10">
