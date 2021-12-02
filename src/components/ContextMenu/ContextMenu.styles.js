2b9879a2eb07e0ec26734be42f026d8fca6e315c
Sammy Guttman <1449@holbertonschool.com>
2021-12-02 18:18:29 -0500

    fix style context menu component

--- src/components/ContextMenu/ContextMenu.styles.js
+++ src/components/ContextMenu/ContextMenu.styles.js
@@ -4,9 +4,4 @@ import Menu from "@mui/material/Menu";
 
 export const StyledMenu = styled(Menu).attrs({
   className: "StyledMenu",
-})`
-  .css-1poimk-MuiPaper-root-MuiMenu-paper-MuiPaper-root-MuiPopover-paper {
-    box-shadow: 0 0 11px rgba(33, 33, 33, 0.05);
-    border-radius: 10px;
-  }
-`;
+})``;
