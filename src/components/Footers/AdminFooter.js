94e8d9e6fbfb7cf717662159ca6ca57217fd0957
samisosa20 <1449@holbertonschool.com>
2022-12-03 11:04:16 -0500

    changes date

--- src/components/Footers/AdminFooter.js
+++ src/components/Footers/AdminFooter.js
@@ -28,7 +28,7 @@ class Footer extends React.Component {
         <Row className="align-items-center justify-content-xl-between">
           <Col xl="6">
             <div className="copyright text-center text-xl-left text-muted">
-              © 2020{" "}
+              © 2022{" "}
               <a
                 className="font-weight-bold ml-1"
                 href="https://github.com/samisosa20"
--- src/components/Footers/AuthFooter.js
+++ src/components/Footers/AuthFooter.js
@@ -30,7 +30,7 @@ class Login extends React.Component {
             <Row className="align-items-center justify-content-xl-between">
               <Col xl="6">
                 <div className="copyright text-center text-xl-left text-muted">
-                  © 2020{" "}
+                  © 2022{" "}
                   <a
                     className="font-weight-bold ml-1"
                     href="https://github.com/samisosa20"
